55608ff0fc261205208b6bc650122bb6f2594540
[dyad] <leandro.antolini@outlook.com>
2025-11-21 14:05:22 -0300

    [dyad] Corrigindo a navegação do botão "Ao Vivo" - wrote 1 file(s)

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@ import ChatListPage from './pages/ChatListPage';
 import ChatPage from './pages/ChatPage';
 import ProfilePage from './pages/ProfilePage';
 import PromotionsPage from './pages/PromotionsPage';
+import LivePage from './pages/LivePage';
 import BottomNav from './components/BottomNav';
 import MatchNotificationModal from './components/MatchNotificationModal';
 import Header from './components/Header';
@@ -54,6 +55,7 @@ const Layout: React.FC = () => {
                                 <Route path="/place/:id" element={<PlaceDetailsPage />} />
                                 <Route path="/promotions" element={<PromotionsPage />} />
                                 <Route path="/match" element={<MatchPage />} />
+                                <Route path="/live" element={<LivePage />} />
                                 <Route path="/chats" element={<ChatListPage />} />
                                 <Route path="/chat/:matchId" element={<ChatPage />} />
                                 <Route path="/profile" element={<ProfilePage />} />
